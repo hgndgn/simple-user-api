92aa423990f5fe517ea20d8fc924370e33445ec9
H.Gndgn <hueseyin.guendogan@gmail.com>
2018-02-20 23:06:21 +0100

    query added

--- src/main/java/api/user/repository/UserRepository.java
+++ src/main/java/api/user/repository/UserRepository.java
@@ -2,6 +2,11 @@ package api.user.repository;
 
 import api.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query("SELECT u FROM User u WHERE :username = u.username")
+    User findUserByUsername(@Param("username") String username);
 }
