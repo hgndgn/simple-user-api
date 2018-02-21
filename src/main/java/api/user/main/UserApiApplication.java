a70396c3da615b8ee04c9806ec644044612ad0ce
H.Gndgn <hueseyin.guendogan@gmail.com>
2018-02-21 15:31:29 +0100

    some data added into data.sql

--- src/main/java/api/user/main/UserApiApplication.java
+++ src/main/java/api/user/main/UserApiApplication.java
@@ -3,9 +3,13 @@ package api.user.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableJpaRepositories("api.user.repository")
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,7 @@
-INSERT INTO USER_ (ID, USERNAME, EMAIL, PASSWORD) VALUES (1, 'john', 'john@doe.com', 'pwd123');
+INSERT INTO USER_ (ID, USERNAME, EMAIL, PASSWORD) VALUES (1, 'john', 'john@domain.com', 'pwd123');
+INSERT INTO USER_ (ID, USERNAME, EMAIL, PASSWORD) VALUES (2, 'mike', 'mike@domain.com', 'pwd123');
+INSERT INTO USER_ (ID, USERNAME, EMAIL, PASSWORD) VALUES (3, 'graham', 'graham@domain.com', 'pwd123');
+INSERT INTO USER_ (ID, USERNAME, EMAIL, PASSWORD) VALUES (4, 'arnold', 'arnold@domain.com', 'pwd123');
+INSERT INTO USER_ (ID, USERNAME, EMAIL, PASSWORD) VALUES (5, 'steven', 'steven@domain.com', 'pwd123');
+INSERT INTO USER_ (ID, USERNAME, EMAIL, PASSWORD) VALUES (6, 'caroline', 'caroline@domain.com', 'pwd123');
+INSERT INTO USER_ (ID, USERNAME, EMAIL, PASSWORD) VALUES (7, 'jordan', 'jordan@domain.com', 'pwd123');
