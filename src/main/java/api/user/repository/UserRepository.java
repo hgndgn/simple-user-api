5753fe926e703d6762ca0a0ff60f7c89ed89b8d4
H.Gndgn <hueseyin.guendogan@gmail.com>
2018-02-21 22:12:55 +0100

    delete by username method added to user repository

--- src/main/java/api/user/repository/UserRepository.java
+++ src/main/java/api/user/repository/UserRepository.java
@@ -2,11 +2,19 @@ package api.user.repository;
 
 import api.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    @Query("SELECT u FROM User u WHERE :username = u.username")
+    @Query("SELECT u FROM User u WHERE u.username = :username")
     User findUserByUsername(@Param("username") String username);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.username = :username")
+    void deleteUserByUsername(@Param("username") String username);
 }
