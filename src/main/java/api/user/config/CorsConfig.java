d9f51e68d786a8ddad70f5c9571d16aa996580e7
H.Gndgn <hueseyin.guendogan@gmail.com>
2018-02-21 18:01:46 +0100

    user model and corsConfig updated

--- src/main/java/api/user/config/CorsConfig.java
+++ src/main/java/api/user/config/CorsConfig.java
@@ -3,17 +3,22 @@ package api.user.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CorsConfig {
+@EnableWebMvc
+public class CorsConfig extends WebMvcConfigurerAdapter {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS");
             }
         };
     }
--- src/main/java/api/user/model/User.java
+++ src/main/java/api/user/model/User.java
@@ -1,15 +1,13 @@
 package api.user.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "USER_")
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @Column(unique = true)
