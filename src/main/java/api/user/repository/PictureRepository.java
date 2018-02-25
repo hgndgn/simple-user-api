10db3e71406252cadebc2d104933f81c33fd043f
H.Gndgn <hueseyin.guendogan@gmail.com>
2018-02-25 17:54:14 +0100

    refactoring

--- src/main/java/api/user/repository/PictureRepository.java
+++
@@ -1,7 +0,0 @@
-package api.user.repository;
-
-import api.user.model.ProfilePicture;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PictureRepository extends JpaRepository<ProfilePicture, Integer> {
-}
