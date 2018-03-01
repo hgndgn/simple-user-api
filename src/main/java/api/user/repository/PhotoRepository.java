e758c4588b2314ae9cf8379e909e467ad42fb83b
H.Gndgn <hueseyin.guendogan@gmail.com>
2018-03-01 21:38:21 +0100

    remove dependent classes of UserPhoto entity

--- src/main/java/api/user/repository/PhotoRepository.java
+++
@@ -1,7 +0,0 @@
-package api.user.repository;
-
-import api.user.model.UserPhoto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhotoRepository extends JpaRepository<UserPhoto, Integer> {
-}
